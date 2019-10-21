11e5252849a52928d16666caa0a0981cffc6f724
masakimurakami11 <monkey_basket_11@i.softbank.jp>
2019-10-21 14:38:38 +0900

    increment_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id:current_user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -15,4 +23,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
